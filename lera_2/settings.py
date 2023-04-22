e9c3ab2aeae932b490726044959a89e2731ed0b3
Dastan Sazanov <84910204+dastankg@users.noreply.github.com>
2023-04-22 21:32:02 +0600

    Update settings.py

--- lera_2/settings.py
+++ lera_2/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+PROJECT_ROOT = os.path.normpath(os.path.dirname(file))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
