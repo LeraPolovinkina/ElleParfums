77a71b5287c4b47812f214a4207b26264756ef2d
nikonych <mamixkik@gmail.com>
2023-05-21 11:09:05 +0600

    ошибка с музыкой фикс

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 from colorfield.fields import ColorField
 from django.db import models
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+import shutil
+
+from django.db.models.signals import post_delete, pre_save
+from django.dispatch import receiver
+
 
 class User(models.Model):
     name = models.CharField(max_length=100)
@@ -15,12 +22,27 @@ class StaticValue(models.Model):
     employee_goal = models.IntegerField(default=100000)
     today_goal = models.IntegerField(default=100000)
     token = models.CharField(max_length=200, default="")
-    date = models.DateField(default=datetime.now())
+    date = models.DateField(default=datetime.now)
     use_start_of_month = models.BooleanField(default=True)
     success_color = ColorField(default="#529653")
     show_users_15_and_20_percent = models.BooleanField(default=True)
     pizza_sound = models.FileField(upload_to='static/sounds/', default='static/sounds/pizza.mp3')
     sale_sound = models.FileField(upload_to='static/sounds/', default='static/sounds/sale.mp3')
     date_turn_on = models.BooleanField(default=True)
+
+
+
     class Meta:
         verbose_name_plural = 'Static Values'
+
+@receiver(pre_save, sender=StaticValue)
+def delete_old_files(sender, instance, *args, **kwargs):
+    if instance.pk:
+        # Получаем предыдущий объект модели
+        old_instance = sender.objects.get(pk=instance.pk)
+        # Если новый файл для поля pizza_sound был загружен и он отличается от предыдущего, то удаляем старый файл
+        if instance.pizza_sound and instance.pizza_sound != old_instance.pizza_sound:
+            old_instance.pizza_sound.delete(save=False)
+        # Если новый файл для поля sale_sound был загружен и он отличается от предыдущего, то удаляем старый файл
+        if instance.sale_sound and instance.sale_sound != old_instance.sale_sound:
+            old_instance.sale_sound.delete(save=False)
